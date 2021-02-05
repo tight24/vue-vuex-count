3ed5d17ff2a6c6d1a5fcf176d1a7e0b2955f1612
taito <rsbull42@gmail.com>
2021-02-06 01:32:32 +0900

    プラスボタンとマイナスボタンのstoreを介した実装

--- src/store/index.js
+++ src/store/index.js
@@ -5,10 +5,24 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    count:5
   },
   mutations: {
+    increment(state){
+      state.count++
+    },
+    decrement(state){
+      state.count--
+    },
+
   },
   actions: {
+    //mutaitionsの処理を呼び出し、5秒後に実行される
+    increment({commit}){
+      setTimeout(
+        commit('increment')
+        ,50000)
+    }
   },
   modules: {
   }
---
+++ src/views/Count.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="count">
+    <h1>This is an count page</h1>
+    {{$store.state.count}}
+    <button @click="increment">+</button>
+    <button @click="decrement">-</button>
+  </div>
+</template>
+<script>
+export default {
+  methods:{
+    increment:function(){
+      this.$store.commit("increment");
+    },
+    decrement:function(){
+      this.$store.commit("decrement");
+    },
+
+  }
+}
+</script>
